6d549d1f77baa995b8f64f71f69c2443a141ce70
GusEstrada <gustavo.medina8124@gmail.com>
2026-05-13 19:32:47 -0600

    fix: update main AZURE_INSIGHTS_CONNECTION_STRING

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,21 @@
-import 'dotenv/config';
-import * as appInsights from 'applicationinsights';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { envs } from './config/envs'; 
+import * as appInsights from 'applicationinsights';
+import { envs } from './config/envs'; // Asegúrate de que esta ruta sea correcta
 
 async function bootstrap() {
-  if (envs.APPINSIGHTS_CONNECTION_STRING) {
-    appInsights
-      .setup(envs.APPINSIGHTS_CONNECTION_STRING)
+  // Cambia APPINSIGHTS_CONNECTION_STRING por AZURE_INSIGHTS_CONNECTION_STRING
+  if (envs.AZURE_INSIGHTS_CONNECTION_STRING) {
+    appInsights.setup(envs.AZURE_INSIGHTS_CONNECTION_STRING)
       .setAutoDependencyCorrelation(true)
       .setAutoCollectRequests(true)
       .setAutoCollectPerformance(true, true)
       .setAutoCollectExceptions(true)
-      .setAutoCollectDependencies(true)
-      .setAutoCollectConsole(true)
-      .setUseDiskRetryCaching(true)
       .start();
-    console.log(' Azure Application Insights iniciado correctamente');
-  } else {
-    console.warn(' Advertencia: APPINSIGHTS_CONNECTION_STRING no encontrada en el .env');
   }
 
   const app = await NestFactory.create(AppModule);
-  
-  app.enableCors();
-
   await app.listen(3000);
-  console.log(' Server running on http://localhost:3000');
+  console.log(`🚀 Server running on http://localhost:3000`);
 }
 bootstrap();
