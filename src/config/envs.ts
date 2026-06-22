3216965c05b1ec4bd449f57b40ed0556eb8b2c1d
GusEstrada <gustavo.medina8124@gmail.com>
2026-06-22 14:59:38 -0600

    Revert "feat: make Redis optional, falls back to in-memory cache"
    This reverts commit 2b2214bbb93d5b298a26dec5e2382b40a5c5049f.

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -7,9 +7,6 @@ export const envs = {
   DB_PASSWORD: process.env.DB_PASSWORD || 'postgres',
   DB_NAME: process.env.DB_NAME || 'pet-radar',
 
-  REDIS_HOST: process.env.REDIS_HOST || '',
-  REDIS_PORT: Number(process.env.REDIS_PORT) || 6379,
-
   MAILER_EMAIL: process.env.MAILER_EMAIL || 'test@test.com',
   MAILER_SECRET_KEY: process.env.MAILER_SECRET_KEY || 'test',
 
